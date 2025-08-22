aef01ded451b72c4b7a5ad8d2353b3be89619bdb
sifatbbillah <sifatbillah82@gmail.com>
2025-08-23 00:06:43 +0600

    2nd

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-// login 
-document.getElementById("loginbtn").addEventListener("click", function(e) {
-    e.preventDefault();
+// login button functionality
+document.getElementById("loginButton").addEventListener("click",function (e){
+    e.preventDefault()
+    const mobileNumber = 12345678910
+    const pin = 1234
+    
+    const mobileNumberValue = document.getElementById
+    ("mobile-number").value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue)
 
-    // correct credentials
-    const mobileNumber = 12345678910;
-    const pinNumber = 1234;
 
-    // user input
-    const mobileNumberValue = document.getElementById("mobile").value;
-    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+    const pinNumberValue = document.getElementById("pin-number").value
 
-    const pinNumberValue = document.getElementById("pin").value;
-    const pinNumberValueConverted = parseInt(pinNumberValue);
+    const pinNumberValueConverted = parseInt(pinNumberValue)
 
-    console.log(mobileNumberValueConverted, pinNumberValueConverted);
-
-    // check login
-    if (mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber) {
-        
-        window.location.href = "./main.html";
-    } else {
-        
-        alert("Invalid mobile number or PIN. Try again.");
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted===pin){
+        window.location.href="./home.html"
     }
-});
+
+    else{
+        alert("Invalid Credentials!")
+    }
+})
